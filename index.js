8928982c18c98b596a3be2b0e5c1a2b7f3003741
yuvrajcell-99 <yyyuvraj11@gmail.com>
2023-03-15 23:52:22 +0530

    ..

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const lib= require("./lib.js");
+
+const express=require('express');
+
+console.log('hello world');
+const server=express();
+
+server.listen(8080);
+
+
+
+
+
+
 //import {sum,diff} from "./lib.js";
 
-const fs = require("fs");
+// const fs = require("fs");
 
-const txt=fs.readFileSync('demo.txt', 'utf8');
+// const txt=fs.readFile('demo.txt', 'utf8',(err,txt)=>{
 
-console.log(txt);
-console.log(lib.sum(4,5),lib.diff(8,6));
-console.log(lib.sum(4,5),lib.diff());
+//     console.log(txt);
+// });
+
+// console.log(lib.sum(4,5),lib.diff(8,6));
+// console.log(lib.sum(4,5),lib.diff());
